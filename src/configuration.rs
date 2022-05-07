e85edc71b9f687a5b1cc5ea2b1247ac11103459c
jason <23511462+jasonnnc3@users.noreply.github.com>
2022-05-07 08:40:08 -0600

    jason/zero2prod-pt6
    Changes committed:
    
    	modified:   Cargo.toml
    	new file:   configuration.yml
    	modified:   src/configuration.rs
    	modified:   src/main.rs

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,3 +1,21 @@
-pub async fn configuration() {
-    todo!()
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub application_port: u16,
+    pub database: DatabaseSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let mut settings = config::Config::default();
+
+    settings.merge(config::File::with_name("configuration"))?;
+    settings.try_into()
 }
