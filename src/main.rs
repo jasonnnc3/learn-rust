8c2af5c2acc28f7a885ff692e2fa7f240cf6db14
jason <23511462+jasonnnc3@users.noreply.github.com>
2022-05-07 05:32:50 -0600

    jason/zero2prod-pt2
    Changes committed:
    
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn healthcheck() -> impl Responder {
     HttpResponse::Ok()
