f07316b9f733ddd70dc3c6f4e82053385f8328d1
jason <23511462+jasonnnc3@users.noreply.github.com>
2022-05-06 22:53:54 -0600

    jason/actix
    Changes committed:
    
    	modified:   Caddyfile
    	modified:   server/src/main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,26 +1,18 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn index() -> impl Responder {
+    "Hello world!"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().service(
+            // prefixes all resources and routes attached to it...
+            web::scope("/actix")
+                // ...so this handles requests for `GET /app/index.html`
+                .route("/", web::get().to(index)),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
