486e1b09d0fe5a64663883e13b2af8ab2b1b285d
jason <23511462+jasonnnc3@users.noreply.github.com>
2022-05-06 18:14:27 -0600

    jason/actix
    Changes committed:
    
    	new file:   server/Cargo.toml
    	new file:   server/src/main.rs
    	new file:   server/target/.rustc_info.json
    	new file:   server/target/CACHEDIR.TAG
    	new file:   server/target/debug/.cargo-lock
    	new file:   server/target/debug/.fingerprint/server-06465cc477c9e049/dep-test-bin-server
    	new file:   server/target/debug/.fingerprint/server-06465cc477c9e049/invoked.timestamp
    	new file:   server/target/debug/.fingerprint/server-06465cc477c9e049/test-bin-server
    	new file:   server/target/debug/.fingerprint/server-06465cc477c9e049/test-bin-server.json
    	new file:   server/target/debug/.fingerprint/server-1eb099794bd26cbe/bin-server
    	new file:   server/target/debug/.fingerprint/server-1eb099794bd26cbe/bin-server.json
    	new file:   server/target/debug/.fingerprint/server-1eb099794bd26cbe/dep-bin-server
    	new file:   server/target/debug/.fingerprint/server-1eb099794bd26cbe/invoked.timestamp
    	new file:   server/target/debug/deps/libserver-06465cc477c9e049.rmeta
    	new file:   server/target/debug/deps/libserver-1eb099794bd26cbe.rmeta
    	new file:   server/target/debug/deps/server-06465cc477c9e049.d
    	new file:   server/target/debug/deps/server-1eb099794bd26cbe.d
    	new file:   server/target/debug/incremental/server-2vqyh977z2az2/s-g9jk853dir-htksub-14m3ivf9luhiy/dep-graph.bin
    	new file:   server/target/debug/incremental/server-2vqyh977z2az2/s-g9jk853dir-htksub-14m3ivf9luhiy/query-cache.bin
    	new file:   server/target/debug/incremental/server-2vqyh977z2az2/s-g9jk853dir-htksub-14m3ivf9luhiy/work-products.bin
    	new file:   server/target/debug/incremental/server-2vqyh977z2az2/s-g9jk853dir-htksub.lock
    	new file:   server/target/debug/incremental/server-mr385xxrue6w/s-g9jk853dje-gmelij-3bmepbbu2be8k/dep-graph.bin
    	new file:   server/target/debug/incremental/server-mr385xxrue6w/s-g9jk853dje-gmelij-3bmepbbu2be8k/query-cache.bin
    	new file:   server/target/debug/incremental/server-mr385xxrue6w/s-g9jk853dje-gmelij-3bmepbbu2be8k/work-products.bin
    	new file:   server/target/debug/incremental/server-mr385xxrue6w/s-g9jk853dje-gmelij.lock

---
+++ server/target/debug/deps/server-06465cc477c9e049.d
@@ -0,0 +1,7 @@
+/Users/jason/Documents/git/learn-rust/server/target/debug/deps/server-06465cc477c9e049.rmeta: src/main.rs
+
+/Users/jason/Documents/git/learn-rust/server/target/debug/deps/server-06465cc477c9e049.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
