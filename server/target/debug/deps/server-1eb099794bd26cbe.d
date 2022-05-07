4db15c89748face076d2c88322fad1279e80a6bc
jason <23511462+jasonnnc3@users.noreply.github.com>
2022-05-06 18:14:57 -0600

    Revert "Revert "jason/actix""
    This reverts commit 73a8b0546561b5ff52a232f2ce30371e8ff1c972.

---
+++ server/target/debug/deps/server-1eb099794bd26cbe.d
@@ -0,0 +1,7 @@
+/Users/jason/Documents/git/learn-rust/server/target/debug/deps/server-1eb099794bd26cbe.rmeta: src/main.rs
+
+/Users/jason/Documents/git/learn-rust/server/target/debug/deps/server-1eb099794bd26cbe.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
