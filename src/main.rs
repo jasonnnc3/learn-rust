c77f2352217ad9d55c22150a218ddfab19924214
jason <23511462+jasonnnc3@users.noreply.github.com>
2022-05-07 05:59:36 -0600

    Zero2prod pt2 - up to 3.5 (pg 45) (#10)
    * jason/zero2prod-pt2
    
    Changes committed:
    
    	modified:   Caddyfile
    	modified:   src/main.rs
    
    * jason/zero2prod-pt2
    
    Changes committed:
    
    	modified:   src/main.rs
    
    * jason/zero2prod-pt2
    
    Changes committed:
    
    	modified:   Cargo.toml
    	new file:   src/lib.rs
    
    * jason/zero2prod-pt2
    
    Changes committed:
    
    	modified:   Cargo.toml
    	modified:   src/lib.rs
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,6 @@
-use actix_web::middleware::Logger;
-use actix_web::{web, App, HttpServer};
-use askama_actix::Template;
-
-#[derive(Template)]
-#[template(path = "hello.html")]
-struct HelloTemplate<'a> {
-    name: &'a str,
-}
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init();
-
-    HttpServer::new(|| {
-        App::new()
-            .wrap(Logger::default())
-            .service(web::resource("/").to(|| async { HelloTemplate { name: "world" } }))
-    })
-    .bind(("127.0.0.1", 4000))?
-    .run()
-    .await
+    run().await
 }
