d98eec404a35b42c5fa2268ab6af8fc22c3ba3b0
jason <23511462+jasonnnc3@users.noreply.github.com>
2022-05-08 03:06:42 -0600

    Zero2prod pt12 - implement tryfrom/tryinto pattern - add testing with generated fake data (pt 198) (#21)
    * jason/zero2prod-pt12
    
    Changes committed:
    
    	modified:   Cargo.toml
    	modified:   src/domain/subscriber_email.rs
    
    * jason/zero2prod-pt12
    
    Changes committed:
    
    	modified:   Cargo.toml
    	modified:   src/domain/subscriber_email.rs
    
    * jason/zero2prod-pt12
    
    Changes committed:
    
    	modified:   src/routes/subscriptions.rs

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -23,6 +23,23 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::SubscriberEmail;
     use claim::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(generated: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(generated);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
+    }
 
     #[test]
     fn empty_string_is_rejected() {
