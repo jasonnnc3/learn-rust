e78032138fb9d32e9842a9017a0beaeeb293335a
jason <23511462+jasonnnc3@users.noreply.github.com>
2022-05-07 23:45:06 -0600

    jason/zero2prod-pt10-digitalocean-finally-maybe
    Changes committed:
    
    	modified:   configuration/local.yml
    	modified:   configuration/production.yml
    	modified:   spec.yml
    	modified:   src/configuration.rs

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,7 @@
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
+use sqlx::ConnectOptions;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -17,6 +18,7 @@ pub struct ApplicationSettings {
 
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
+    pub require_ssl: bool,
     pub username: String,
     pub password: Secret<String>,
     #[serde(deserialize_with = "deserialize_number_from_string")]
@@ -27,15 +29,23 @@ pub struct DatabaseSettings {
 
 impl DatabaseSettings {
     pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
             .password(self.password.expose_secret())
             .port(self.port)
+            .ssl_mode(ssl_mode)
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
-        self.without_db().database(&self.database_name)
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing::log::LevelFilter::Trace);
+        options
     }
 }
 
