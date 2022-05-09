17b4cb62627d5efbd9fbcd15e040367e068b538d
jason <23511462+jasonnnc3@users.noreply.github.com>
2022-05-09 06:07:41 -0600

    custom rustfmt settings (spaces > tabs, 2 > 4) (#36)
    * jason/zero2prod-pt27
    
    Changes committed:
    
    	modified:   tests/api/subscription_confirm.rs
    
    * jason/zero2prod-pt27
    
    Changes committed:
    
    	new file:   rustfmt.toml
    	modified:   src/configuration.rs
    	modified:   src/domain/new_subscriber.rs
    	modified:   src/domain/subscriber_email.rs
    	modified:   src/domain/subscriber_name.rs
    	modified:   src/email_client.rs
    	modified:   src/main.rs
    	modified:   src/routes/health_check.rs
    	modified:   src/routes/subscriptions.rs
    	modified:   src/routes/subscriptions_confirm.rs
    	modified:   src/startup.rs
    	modified:   src/telemetry.rs
    	modified:   tests/api/health_check.rs
    	modified:   tests/api/helpers.rs
    	modified:   tests/api/subscription_confirm.rs
    	modified:   tests/api/subscriptions.rs
    
    * jason/zero2prod-pt27
    
    Changes committed:
    
    	modified:   rustfmt.toml
    	modified:   src/configuration.rs
    	modified:   src/email_client.rs
    	modified:   src/routes/subscriptions.rs
    	modified:   src/startup.rs
    	modified:   src/telemetry.rs

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -4,56 +4,56 @@ use validator::validate_email;
 pub struct SubscriberEmail(String);
 
 impl SubscriberEmail {
-    pub fn parse(value: String) -> Result<SubscriberEmail, String> {
-        if validate_email(&value) {
-            Ok(Self(value))
-        } else {
-            Err(format!("{} is not a valid subscriber email.", value))
-        }
+  pub fn parse(value: String) -> Result<SubscriberEmail, String> {
+    if validate_email(&value) {
+      Ok(Self(value))
+    } else {
+      Err(format!("{} is not a valid subscriber email.", value))
     }
+  }
 }
 
 impl AsRef<str> for SubscriberEmail {
-    fn as_ref(&self) -> &str {
-        &self.0
-    }
+  fn as_ref(&self) -> &str {
+    &self.0
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::SubscriberEmail;
-    use claim::assert_err;
-    use fake::faker::internet::en::SafeEmail;
-    use fake::Fake;
+  use super::SubscriberEmail;
+  use claim::assert_err;
+  use fake::faker::internet::en::SafeEmail;
+  use fake::Fake;
 
-    #[derive(Debug, Clone)]
-    struct ValidEmailFixture(pub String);
+  #[derive(Debug, Clone)]
+  struct ValidEmailFixture(pub String);
 
-    impl quickcheck::Arbitrary for ValidEmailFixture {
-        fn arbitrary<G: quickcheck::Gen>(generated: &mut G) -> Self {
-            let email = SafeEmail().fake_with_rng(generated);
-            Self(email)
-        }
+  impl quickcheck::Arbitrary for ValidEmailFixture {
+    fn arbitrary<G: quickcheck::Gen>(generated: &mut G) -> Self {
+      let email = SafeEmail().fake_with_rng(generated);
+      Self(email)
     }
+  }
 
-    #[quickcheck_macros::quickcheck]
-    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
-        SubscriberEmail::parse(valid_email.0).is_ok()
-    }
+  #[quickcheck_macros::quickcheck]
+  fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+    SubscriberEmail::parse(valid_email.0).is_ok()
+  }
 
-    #[test]
-    fn empty_string_is_rejected() {
-        let email = "".to_string();
-        assert_err!(SubscriberEmail::parse(email));
-    }
-    #[test]
-    fn email_missing_at_symbol_is_rejected() {
-        let email = "ursuladomain.com".to_string();
-        assert_err!(SubscriberEmail::parse(email));
-    }
-    #[test]
-    fn email_missing_subject_is_rejected() {
-        let email = "@domain.com".to_string();
-        assert_err!(SubscriberEmail::parse(email));
-    }
+  #[test]
+  fn empty_string_is_rejected() {
+    let email = "".to_string();
+    assert_err!(SubscriberEmail::parse(email));
+  }
+  #[test]
+  fn email_missing_at_symbol_is_rejected() {
+    let email = "ursuladomain.com".to_string();
+    assert_err!(SubscriberEmail::parse(email));
+  }
+  #[test]
+  fn email_missing_subject_is_rejected() {
+    let email = "@domain.com".to_string();
+    assert_err!(SubscriberEmail::parse(email));
+  }
 }
