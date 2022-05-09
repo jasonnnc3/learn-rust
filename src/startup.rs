5937d679c0d9ee0fe2c8a2282d41a0465c095323
jason <23511462+jasonnnc3@users.noreply.github.com>
2022-05-08 22:22:13 -0600

    jason/zero2prod-pt21
    Changes committed:
    
    	modified:   configuration/local.yml
    	modified:   spec.yml
    	modified:   src/configuration.rs
    	modified:   src/routes/mod.rs
    	modified:   src/routes/subscriptions.rs
    	new file:   src/routes/subscriptions_confirm.rs
    	modified:   src/startup.rs
    	modified:   tests/api/helpers.rs
    	modified:   tests/api/main.rs
    	new file:   tests/api/subscription_confirm.rs

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{health_check, subscribe};
+use crate::routes::{confirm, health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::http::StatusCode;
 use actix_web::web::Data;
@@ -35,7 +35,12 @@ impl Application {
         );
         let listener = TcpListener::bind(&address)?;
         let port = listener.local_addr().unwrap().port();
-        let server = run(listener, connection_pool, email_client)?;
+        let server = run(
+            listener,
+            connection_pool,
+            email_client,
+            configuration.application.base_url,
+        )?;
         Ok(Self { port, server })
     }
     pub fn port(&self) -> u16 {
@@ -59,21 +64,28 @@ pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
         .connect_lazy_with(configuration.with_db())
 }
 
+pub struct ApplicationBaseUrl(pub String);
+
 pub fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
+    base_url: String,
 ) -> Result<Server, std::io::Error> {
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
+    let base_url = Data::new(ApplicationBaseUrl(base_url));
+
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home_page))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
+            .app_data(base_url.clone())
     })
     .listen(listener)?
     .run();
